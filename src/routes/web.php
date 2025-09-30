1c49550d7d174c7e5c9ac69caa863bb6fbce2555
nico <yuuuuuuuu.13@gmail.com>
2025-09-30 13:40:06 +0900

    ルーティング作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,15 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/products', [ProductController::class, 'index'])->name('index');
+
+Route::get('/products', [ProductController::class, 'index'])->name('index'); //商品一覧
+Route::get('/products/register', [ProductController::class, 'create']); //商品登録
+Route::post('/products/register', [ProductController::class, 'store']); //保存
+Route::get('/products/{productId}', [ProductController::class, 'show']); //商品詳細
+Route::get('/products/search', [ProductController::class, 'search']); //検索
+Route::patch('/products/{productId}/update', [ProductController::class, 'update']); //商品更新
+Route::delete('/products/{productId}/delete', [ProductController::class, 'destroy']); //削除
+
+
