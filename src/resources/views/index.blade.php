ff0ac98f90c6c636ca87936bceaaa711ff9dda8c
nico <yuuuuuuuu.13@gmail.com>
2025-10-05 23:48:48 +0900

    css編集

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -45,7 +45,7 @@
             @foreach ($products as $product)
             <div class="product-card">
                 <a class="product-card__link" href="{{ route('products.show', ['productId' => $product->id]) }}">
-                <img src="{{ $product->image_url }}" alt="{{ $product->name }}">
+                <img src="{{ $product->image_url }}" alt="{{ $product->name }}" class="product-card__image">
 
                 <div class="product-card__info">
                     <p class="product-card__name">{{$product->name}}</p>
--- src/resources/views/show.blade.php
+++ src/resources/views/show.blade.php
@@ -14,54 +14,54 @@
 
 
         </div>
+        <div class="actions-row">
+            {{-- 更新フォーム --}}
+            <form id="form-update" class="form" action="{{ route('products.update', ['productId' => $product->id]) }}" method="post" enctype="multipart/form-data">
+                @method('PATCH')
+                @csrf
+                <div class="form__grid">
+                    <div class="form__grid-item">
+                        <div class="form__grid-image">
+                            <img id="pePreview" src="{{ $product->image_url }}" alt="preview">
 
-        {{-- 更新フォーム --}}
-        <form id="form-update" class="form" action="{{ route('products.update', ['productId' => $product->id]) }}" method="post" enctype="multipart/form-data">
-            @method('PATCH')
-            @csrf
-            <div class="form__grid">
-                <div class="form__grid-item">
-                    <div class="form__grid-image">
-                        <img id="pePreview" src="{{ $product->image_url }}" alt="preview">
-
-                    </div>
-                    <label class="form__grid-upload">
-                        <input type="file" name="image" accept="image/*" id="peImageInput" class="form__grid-upload__input">
-                        <span class="form__grid-upload__btn">ファイルを選択</span>
-                        <span id="peFileName" class="form__grid-upload__name">
-                            {{ $product->image ? basename($product->image) : '選択されていません' }}
-                        </span>
-                    </label>
-                    @error('image') <p class="pe-error">{{ $message }}</p> @enderror
-                </div>
-
-                <div class="form-item">
-                    <div class="form-item-field">
-                        <label class="form-item-field-label">商品名</label>
-                        <input type="text" name="name" value="{{ old('name', $product->name) }}" class="form-item-field-input">
-                        @error('name') <p class="pe-error">{{ $message }}</p> @enderror
+                        </div>
+                        <label class="form__grid-upload">
+                            <input type="file" name="image" accept="image/*" id="peImageInput" class="form__grid-upload__input">
+                            <span class="form__grid-upload__btn">ファイルを選択</span>
+                            <span id="peFileName" class="form__grid-upload__name">
+                                {{ $product->image ? basename($product->image) : '選択されていません' }}
+                            </span>
+                        </label>
+                        @error('image') <p class="pe-error">{{ $message }}</p> @enderror
                     </div>
 
-                    <div class="form-item-field">
-                        <label class="form-item-field-label">値段</label>
-                        <input type="text" name="price" value="{{ old('price', $product->price) }}" class="form-item-field-input">
-                        @error('price') <p class="pe-error">{{ $message }}</p> @enderror
-                    </div>
+                    <div class="form-item">
+                        <div class="form-item-field">
+                            <label class="form-item-field-label">商品名</label>
+                            <input type="text" name="name" value="{{ old('name', $product->name) }}" class="form-item-field-input">
+                            @error('name') <p class="pe-error">{{ $message }}</p> @enderror
+                        </div>
 
-                    <div class="form-item-field">
-                        <label class="form-item-field-label">季節</label>
-                        <div class="season">
-                            @foreach($seasons as $season)
-                            <label>
-                                <input class="form-season" type="checkbox" name="seasons[]" value="{{ $season->id }}">
-                                {{ $season->name }}
-                            </label>
-                            @endforeach
-                            @error('season') <p class="form__error">{{ $message }}</p> @enderror
+                        <div class="form-item-field">
+                            <label class="form-item-field-label">値段</label>
+                            <input type="text" name="price" value="{{ old('price', $product->price) }}" class="form-item-field-input">
+                            @error('price') <p class="pe-error">{{ $message }}</p> @enderror
+                        </div>
+
+                        <div class="form-item-field">
+                            <label class="form-item-field-label">季節</label>
+                            <div class="season">
+                                @foreach($seasons as $season)
+                                <label>
+                                    <input class="form-season" type="checkbox" name="seasons[]" value="{{ $season->id }}">
+                                    {{ $season->name }}
+                                </label>
+                                @endforeach
+                                @error('season') <p class="form__error">{{ $message }}</p> @enderror
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
                 <div class="form-item2">
                     <div class="form-item-field-description">
                         <label class="form-item-field-label">商品説明</label>
@@ -70,20 +70,21 @@
                     </div>
                 </div>
 
-            {{-- ボタン行 --}}
-            <div class="form__btn">
-                <a href="{{ route('index') }}" class="form__btn-back">戻る</a>
-                <div class="form-actions__btn">
-                    <button type="submit" class="form__btn-update">変更を保存</button>
-                </div>
-        </form>
-        <form action="{{ route('products.delete',['productId' => $product->id])}}" method="post" class="delete-form">
-            @csrf
-            @method('DELETE')
+                {{-- ボタン行 --}}
+                <div class="form__btn">
+                    <a href="{{ route('index') }}" class="form__btn-back">戻る</a>
+                    <div class="form-actions__btn">
+                        <button type="submit" class="form__btn-update">変更を保存</button>
+                    </div>
+            </form>
+            <form action="{{ route('products.delete',['productId' => $product->id])}}" method="post" class="delete-form">
+                @csrf
+                @method('DELETE')
                 <div class="delete-form__button">
                     <button type="submit" class="delete-form__buttonーbtn" title="削除">🗑</button>
                 </div>
-        </form>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
